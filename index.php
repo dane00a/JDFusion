7af24655f20a6c7e8ee3f8004cb92bfe7b4cc08f
dane00a <dane.andrews99@gmail.com>
2015-07-01 01:02:09 -0500

    Initial Commit
    WIP Menu; Basic Backend

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+  <head>
+    <link src="css/index.css" rel="stylesheet" type="text/css"/>
+    <!--<script src="//code.jquery.com/jquery-latest.min.js"></script>
+    <script src="//unslider.com/unslider.min.js"></script>-->
+  </head>
+  <body>
+    <?php include('menu.html'); ?>
+    <div class="wrapper">
+      <div class="cont white">
+
+      </div>
+      <div class="cont purp">
+
+      </div>
+      <div class="cont white">
+
+      </div>
+      <div class="cont black">
+
+      </div>
+    </div>
+  </body>
+</html>
