b29f25804030bf3d1ecac3f39ce632fe52a114a8
Dane <dane.andrews99@gmail.com>
2015-07-04 00:10:01 -0500

    New Resources; Front Page Incomplete
    Added media; front page coming to fruition; backend optimization and
    trying to get a more responsive design

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
   <head>
-    <link src="css/index.css" rel="stylesheet" type="text/css"/>
+    <link href="css/index.css" rel="stylesheet" type="text/css">
     <!--<script src="//code.jquery.com/jquery-latest.min.js"></script>
     <script src="//unslider.com/unslider.min.js"></script>-->
   </head>
@@ -8,19 +8,56 @@
     <?php include('menu.html'); ?>
     <div class="wrapper">
       <div class="cont white">
-        <h1>Welcome</h1>
-        <p>
-          Welcome to the future of Do-It-Yourself Fusion.
-        </p>
+        <div class="subcont">
+          <h1>Welcome</h1>
+        <div>
+          <p>
+            Welcome to the future of Nuclear Fusion.
+          </p>
+          <p>
+            We're two "Fusioneers", as the term was dubbed; pioneering science students on a mission to create more than a few Facebook posts and earn a few trophies throughout High School. So we pursue Nuclear Fusion, and our demo reactor worked.
+          </p>
+          <p>
+            We want to do a lot more than just that. This site is made to chronicle those endeavours.
+          </p>
+        </div>
+      <!--    <img src="images/nuclear-fusion.jpg" alt="Nuclear_fusion_intro"/> -->
       </div>
       <div class="cont purp">
-
+        <div class="subcont">
+        <h1>We Have Already Begun</h1>
+          <div class="cont2txt">
+            <p>
+              Our Demo Model of the Farnsworth-Hirsch Fusor tested successfully, and created plasma using Ohm's Law and Inertial Electrostatic Confinement - More on that on the <a href="fusor.php"> Farnsworth-Hirsch Fusor Page</a>
+            </p>
+            <p>
+              See the media to the right of a test of our Fusor.
+            </p>
+            <p>
+              After some small refinements, we have now preserved this model and moved on to our next project: the Polywell.
+            </p>
+        </div>
+        <div class="vidcont">
+          <video class="intro_vid" controls >
+            <source src="videos/vid1.mp4" type="video/mp4">
+              Your browser does not support video; please upgrade it.
+          </video>
+        </div>
+        </div>
       </div>
       <div class="cont white">
-
+        <div class="subcont">
+        <p>
+          aids
+        </p>
+      </div>
       </div>
       <div class="cont black">
-
+        <div class="subcont">
+        <p>
+          Test
+        </p>
+      </div>
       </div>
     </div>
   </body>
