be6a77c224594b5a800f0506d234e3519afc38de
Dane <dane.andrews99@gmail.com>
2015-07-11 00:17:39 -0500

    First Draft of Front Page
    First Draft of Front Page is done, and mobile responsiveness works for
    screens >=450px wide. So, basically anything.

--- index.php
+++ index.php
@@ -22,12 +22,16 @@
             We want to do a lot more than just that. This site is made to chronicle those endeavours.
           </p>
         </div>
+      </div>
       <!--    <img src="images/nuclear-fusion.jpg" alt="Nuclear_fusion_intro"/> -->
       </div>
       <div class="cont purp">
         <div class="subcont">
         <h1>We Have Already Begun</h1>
           <div class="cont2txt">
+            <p>
+              The Farnsworth-Hirsch Fusor was our initial speadhead into nuclear fusion by creating plasma, the step down from creating heavier elements. It was a rewarding if not occasionally hectic ride, and contributed immensely to the knowledge and experience needed to begin the Polywell projet.
+            </p>
             <p>
               Our Demo Model of the Farnsworth-Hirsch Fusor tested successfully, and created plasma using Ohm's Law and Inertial Electrostatic Confinement - More on that on the <a href="fusor.php"> Farnsworth-Hirsch Fusor Page</a>
             </p>
@@ -51,19 +55,36 @@
         <h1>The Polywell</h1>
         <img src="images/polywell-diagram.png" alt="Polywell" class="polywell_img">
         <p>
-          We've begun working on a reactor heavily based off of the Polywell design, a reactor based upon electromagnetism and electron wells. Currently, we're in the final planning stages of the project; building will begin soon on the second amatuer Polywell Nucelar Reactor in existance. <br>
+          We've begun working on a reactor heavily based off of the Polywell design, a reactor based upon electromagnetism and electron wells. Currently, we're in the final planning stages of the project; building will soon commence on the second amatuer Polywell Nucelar Reactor in existance. <br>
         </p>
         <p>
-          We're very excited for that prospect, however you could also have some involvement as well.
+          This project is even larger and more complicated of a milestone to conquer, and the clock is ticking. Nuclear fusion is at our fingertips, and we plan to make it a reality before high school ends.
+        </p>
+        <p>
+          We're very excited for that prospect, to say the least. Amazing things lay around the corner for this project.
         </p>
       </div>
       </div>
       <div class="cont black">
         <div class="subcont">
-        <p>
-          Test
-        </p>
-      </div>
+          <h1>Interesting...</h1>
+          <div>
+            <p>
+              That it is. This project is a pioneer in Do-It-Yourself Nuclear Fusion, and is a difficult and ambitious effort.
+            </p>
+            <p>
+              However, no great effort can come into fruition without help. We've conversed with eneryone from graduate students to electricians, contractors to entreprenuers, and other men and women of various occupations.
+            </p>
+            <p>
+              But we're not finished. We need you, dear reader. You.
+            </p>
+              <p>
+                And if you find this new revolution in science fascinating, feel free to browse the site and learn. Spread the word. Donate to our Kickstarter, if you feel compelled.
+              </p>
+              <p>
+                And finally, thanks for reading. Giving your time is one of the most improtant assets you just gave. Thank you.
+              </p>
+          </div>
       </div>
     </div>
   </div>
