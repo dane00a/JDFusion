9aaec49b55fc8104c24a6d91aac9f35b57800f36
Dane <dane.andrews99@gmail.com>
2015-07-23 13:23:46 -0500

    Blog Page Generation and Connecting Variables and MooTools
    Added MooTools, but am unaware if implementation is an option. May
    remove later.
    
    Progress on Polywell DB, but its failing; rec. archive the code and
    begin page generation method on Submission of Webmaster Page instead;
    will be more feasible. Stack Overflow hasn't yielded much.

--- polywell.php
+++ polywell.php
@@ -29,7 +29,6 @@
       // Return the number of rows in result set
       $rowcount=mysqli_num_rows($result);
       }
-
     //this variable must increment when looping through the db for various items. yeah
     for ($sqlindex = 0; $sqlindex <= $rowcount; $sqlindex++){
       if ($result = $conn->query("SELECT * FROM writeups LIMIT ".$sqlindex.", 1;")){
@@ -39,17 +38,40 @@
           $titlecontent =  $row->title;
           $writeupcontent = $row->body;
           $rowdateandtime = $row->dateandtime;
+          $article_number = $sqlindex + 1;
           echo $str=<<<ARTICLE
           <div class="article">
             <div class="titleanddate">
               <h2>$titlecontent</h2>
-              <h3>Written $rowdateandtime</h3>
+              <h3 class="dtartnum">Written $rowdateandtime, Article Number $article_number</h3>
             </div>
             <div class="truncate">
               <p class="writeup">
                 $writeupcontent
               </p>
-              <a href="templatepage.php">Read More...</a>
+              <a href="templatepage.php" onmouseenter="assignCookie()">Read More...</a>
+              <script>
+              function assignCookie(){
+                //total nr in list
+               window.alert(document.getElementsByClassName("article").length);
+                //pos in list and definition of variable per button
+                var sqlindex = $sqlindex;
+                for (i=0; i<=sqlindex;i++){
+                  var name = i;
+                  this [name] = $sqlindex;
+                  window.alert(name);//how do we get this to work?
+                }
+              /* This code will be developed later, I suppose... need to fix the above
+               window.alert(dtartnum);
+                var pattern = /Article Number \d{1,3}/;
+                  if(pattern.test(document.getElementsByClassName(dtartnum).value)){
+                    window.alert(pattern.test(document.getElementsByClassName(dtartnum).value));
+                  }
+                  else{
+                    window.alert(getElementsByClassName(dtartnum).value);
+                  }*/
+              }
+              </script>
             </div>
           </div>
 ARTICLE;
