abc0f413f0698316afa3d8744bc3f9ee2f4e1e39
dane00a <dane.andrews99@gmail.com>
2015-09-02 23:46:44 -0500

    Redux of Database
    I find it may be better to embed Google Docs than deal with all the BS
    involving formattting with a database.
    SO THAT'S WHAT I'VE BEGUN.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# JDFusion
-DA
+# A^2 Fusion
+This is our Github repo for our website located @ <insert link here>.
--- aboutus.php
+++ aboutus.php
@@ -6,6 +6,7 @@
   <body>
     <?php
       include("menu.html");
-     ?>
+      ?>
+    <iframe src="https://docs.google.com/document/d/1r7mSSSpSx0zPdkSqxWFKn55E5tzZZAGvxOKZlXynVb0/pub?embedded=true"></iframe>
   </body>
 </html>
