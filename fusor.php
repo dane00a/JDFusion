b36513369f0c9df1004cf35acf008634437d1918
dane00a <dane.andrews99@gmail.com>
2016-04-11 00:25:26 -0500

    Fusor Text and License
    - Changed Fusor page
    - Add WTFYW license, since it has been suggested I license the code in
    some form

--- fusor.php
+++ fusor.php
@@ -10,31 +10,20 @@
         <div class="fusor_intro">
           <h1>The Farnsworth-Hirsch Fusor: An Introduction</h1>
           <p>
-            The Farnsworth-Hirsch Fusor was one of our first steps into nuclear fusion. As such, we've become enamored with it and the tumultuous journey it took to make it.
+            The Farnsworth-Hirsch Fusor was our first endeavor in pursuing nuclear fusion. The idea to collaborate over the building of a reactor began during a free period in high school at the end of our freshman year. Initially it seemed farfetched, but after digging into articles and convincing our parents it could be done safely, the building of it soon became a reality.
           </p>
           <p>
-            We (Dane Andrews and Jeremy Adams) initially conceieved the idea of pursuing Nuclear Fusion dring a free period in high school. It was early June at the time, and school was going to be let out soon.
+            Much of the building took place over the summer along with the learning of several fundamental fusion concepts needed to make our reactor. We faced challenges and overcame them. The circuit was the toughest to accomplish because we had no prior electrical experience and this slowed our progress near the end of our building.
           </p>
           <p>
-            We decided that we'd do some work on nuclear fusion, and it didn't take long to find a few articles about the Fusor. From there, we spent most of July, August, and the beginning of the school year working
-            on the reactor.
-          </p>
-          <p>
-            We eventually hit a bit of a lull in the working between early October and Thanksgiving, when work resumed with a clamor and a bang. As sort of a late Christmas present, we achieved our end result, Plasma, on January 2nd, 2015, after resolving one of the last circuitry issues.
-          </p>
-          <p>
-            That was a great day. Read on to find out more about specific aspects of the project.
+            We achieved our first plasma on January 2nd, 2015, after resolving the last circuitry issues. We then tested different electrodes and multiple plasmas were achieved before we decided to retire our demo reactor and take up a much more challenging approach.
           </p>
         </div>
       <hr>
         <div class="fusor_circuitry">
           <h1>The Circuitry</h1>
           <p>
-            For our circuit, AC and DC current became one of the largest hurdles to overcome.
-          </p>
-          <p>
-            For our chamber, we first needed to raise the voltage as high as possible without doing damage to the chamber, which put us in the range of 6-12kV (kilovolts; 1000 V= 1kV). We settled on a Neon Sign Transformer (NST), which took the power from the standard 120VAC wall outlet and output
-            6.5kV at 30 mA (milliamps, 1000mA = 1 A).
+            We needed a high voltage electrode to ionize air under vacuum conditions. This meant an electrode with a voltage bias in the range of 6-12kV was needed according to Paschen’s curve, which gives the necessary voltage needed for a breakdown or discharge to occur as a function of pressure. We settled on a Neon Sign Transformer (NST), which took the power from the standard 120VAC wall outlet and output 6.5kV at 30 mA.
           </p>
           <div class="imgcontfusor">
             <div class="img_div">
@@ -50,7 +39,7 @@
               </p>
             </div>
           </div>
-          <div class="imgcontfusor">
+          <!--<div class="imgcontfusor">
             <div class="img_div">
               <img src="images/acgraph.gif" alt="AC Current">
               <p>
@@ -63,36 +52,22 @@
                  Direct Current and Rectifications
               </p>
             </div>
-          </div>
+          </div>-->
           <p>
-            However, there was a slight problem: the NST's output was in AC (Alternating Current). What this meant is that the current (and voltage, becuase they are directly proportional) wouldn't come out at 6.5kV and 30mA all the time; it would "pulse" 50/60 times (common measured in Hertz, Hz) along a sine wave.
-            This created a problem because it would cause a voltage dip well below 6.5kV, which would have rendered the circuit nearly useless as the dips would allow the plasma to dissapate inside the chamber.
+              The NST was outputting an alternating current and this was problematic because the multidirectional current would not be as effective in creating the electrical discharge needed to ionize the air (mainly nitrogen) in our chamber.
           </p>
           <p>
-            We eventually decided to use a Full-Wave Rectifier to prevent the DC current from dropping below the center line of the sine graph to keep the voltage at or above 6.5kV at any given time. We did not feel the need to use a smoothing capacitor, however, so we just had the top half of the sine wave because smoothing capacitors at that voltage were expensive and unnecessary for our reactor.
-          </p>
-          <p>
-            In order to make it, we needed to use a special high-voltage wire with a lot of insulation, as well as four diodes to create the rectifier. We went and asked our engineering teacher for some soldering equipment, and to our pleasure, he obliged. We cut the insulated wires and soldered them into a diamond-shape, with one diode on each side. The diodes were paired to be emanating from one point, only to converge on the opposite
-            side of the Rectifier to properly rectify the Alternating Current.
-          </p>
-          <p>
-            Afterwards, we took the negative output of the Full Wave Rectifier and connected it to the electrode, and connected the positive end to the Aluminum Plate on the opposite side of the chamber. After that, the circuit was complete.
+              We eventually decided to build a full-wave rectifier to prevent the DC current from dropping below the center to keep the voltage at or above 6.5kV at any given time. We did not feel the need to use a smoothing capacitor because they were expensive and unnecessary for our circuits conditions. We used a well-insulated high-voltage wire, and four diodes to create our rectifier. We soldered the diodes and the wires together in a diamond shape as shown in the circuit diagram of the full-wave rectifier. The outputs of our rectifier were fed into our chamber to our electrodes through some ceramic and epoxy feedthroughs.
           </p>
         </div>
       <hr>
         <div class="fusor_chamber">
           <h1>The Chamber</h1>
             <p>
-              For the chamber, we needed something durable and a material that could withstand going under a vacuum during the creation of the plasma. After searching around through a bunch of materials, we decided on using a borosilicate glass cylinder, primarily because it had the aforementioned features as well as having a very low coefficient for thermal expansion. This would allow it withstand significantly more amounts of heat than other competing materials without needing to expand.
+              For our chamber we needed something durable and that could withstand going under a vacuum during the creation of a plasma. We decided to use a borosilicate glass cylinder placed between two aluminum plates with feedthroughs for our electrode. Borosilicate glass has a low coefficient for thermal expansion which allows it withstand significantly more amounts of heat than other competing materials, making it suitable for use in our chamber. The aluminum plates around the glass were machined and gaskets were placed between the glass and aluminum to create a low pressure environment when our vacuum was used.
             </p>
             <p>
-              Afterwords, we obtained and cut two aluminum plates to cover the exposed sides of the borosilicate cylinder. We drilled holes into the plates, and bound them together with large screws to help give a more substantial seal. We also drilled a hole in the bottom of the chamber to fit the ceramic rod and screw into the chamber that would later hold the electrode.
-            </p>
-            <p>
-              We had three electrodes: a primary one made of tungsten, and two secondary ones made of copper. The tungsten and one of the copper electrodes were made in a spherical shape, and the third electrode was crafted into a cubic shape.
-            </p>
-            <p>
-              Finally, a hole was drilled into one of the aluminum plates to fit the vacuum into the chamber.
+              We had multiple electrodes constructed for use in our chamber. One was made of tungsten, another made from aluminum, and several copper electrodes were also designed, ranging in size and shape. Some of them were spherical while others were shaped closer to a cube or pyramid. Most of the electrodes were at some point tested in our reactor. Generally the spherically shaped grids had the most success at achieving a large plasma.
             </p>
             <div class="img_div heightmod">
               <image src="images/fusor_electrode.png" alt="Fusor Electrode"></image>
@@ -111,15 +86,15 @@
           <div class="fusor_test">
             <h1>Testing the Fusor</h1>
             <p>
-              On January 2, 2015, we finally cleared up an issue with the circuit regarding grounding one of the terminals, and we were able get it to work under the tungsten electrode.
+              Multiple tests were run with different electrodes positioned to optimize for maximum voltage breakdown in our chamber. Most experiments failed to achieve a large plasma, but some spherical grids were successful as shown below. Our first plasma took place on January 2nd, 2015, with a discharge between the positive terminal grounded on the aluminum wall of our chamber and the negative electrode inside our chamber. This yielded a very small plasma which was later improved upon by inserting another grid into our chamber so two spherical grids would create more even electrical arching. 
             </p>
             <p>
-              Afterwards, we tested out the copper electrodes (both of which were successful). Soon after, we archived the project and began work on the Polywell.
+              Challenges were seen in our vacuum’s capabilities in creating a low pressure environment for our chamber. We used a dual stage 5CFM roughing pump which provided some negative pressure but leaks in our chamber also caused problems with pressure. An effective pressure gauge was not purchased so the actual pressure of our chamber was never quite determined, but by observing the plasma it was obvious that with a lower pressure better results could have been seen. However, a balance needs to be maintained when making a fusor without deuterium because a certain amount of air needs to stay in the chamber for plasma to be created.
             </p>
             <div class="img_div">
               <image src="images/fusor_test.png" alt="Fusor Test"></image>
               <p>
-                Fusor With Tungsten Electrode
+                A video of a plasma we achieved in our demo reactor is shown below.
               </p>
             </div>
             <div class="vidcont">
